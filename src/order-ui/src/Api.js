34b35b2667097dddb627673fb239bd361f8c5625
Miroslav Zniscinskij <mirekas123@gmail.com>
2023-12-08 04:07:19 +0100

    fetch resource from supply

--- src/order-ui/src/Api.js
+++ src/order-ui/src/Api.js
@@ -1,13 +1,13 @@
-const BASE_URL = 'http://localhost:8090';
+const ORDER_BASE_URL = 'http://localhost:8090';
 
 export const fetchData = async () => {
-    const response = await fetch(`${BASE_URL}/api/order`);
+    const response = await fetch(`${ORDER_BASE_URL}/api/order`);
     const data = await response.json();
     return data;
 };
 
 export const postData = async (newForecast) => {
-    const response = await fetch(`${BASE_URL}/api/order`, {
+    const response = await fetch(`${ORDER_BASE_URL}/api/order`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -18,4 +18,15 @@ export const postData = async (newForecast) => {
     if (!response.ok) {
         throw new Error(`Failed to add order: ${response.status}`);
     }
+};
+
+const SUPPLY_BASE_URL = 'http://localhost:9093';
+
+export const fetchResourceData = async () => {
+    const response = await fetch(`${SUPPLY_BASE_URL}/resource`, {
+        method: 'GET',
+        credentials: 'include',
+    });
+    const data = await response.json();
+    return data;
 };
